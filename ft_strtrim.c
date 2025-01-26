9c1367c9eb17482b08c10b51edb699a765ff2caf
DIegoR <128084117+DLRedd@users.noreply.github.com>
2025-01-26 14:18:59 +0100

    jjj

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dierojas <dierojas@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 21:29:38 by dierojas          #+#    #+#             */
-/*   Updated: 2025/01/26 00:56:42 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/01/26 14:14:08 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,33 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!set || !s1)
 		return (NULL);
 	start = 0;
-	end = ft_strlen(s1);
-
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	if (ft_strlen(s1) == ft_strlen(set))
 		return (NULL);
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	len = end - start;
+	s2 = malloc((len + 1) * (sizeof (char)));
+	if (!s2)
+		return (NULL);
+//--------------------------------------------------------------------------------------------//
+	while (s1[len] && s1[len] != s1[end])//le tengo que marcar la parte final del coso
+	{
+		s2[start] = s1[len];
+		len++;
+	}
+	s2[len] = '\0';
+	return (s2);
+}
 
-// 	while (s1[i] && s1[i] != set[ft_strlen(set - 1)])
-// 	{
-// 		i--;
-// 		end--;
-// 	}
-// 	s2 = malloc((end - start + 1) * (sizeof (char)));
-// 	if(!s2)
-// 		return (NULL);
-// 	while (s1[end - start] == s2[i])
-// 	{
-// 		s2[i] = s1[start - end];
-// 		i++;
-// 	}
-// 	s2[i] = '\0';
-// 	return (s2);
-// }
+#include <stdio.h>
 
-// #include <stdio.h>
+int	main ()
+{
+	printf("%s\n", ft_strtrim("HolaMundo", "undo"));
+	printf("%s\n", ft_strtrim("HolaMundo", "Hola"));
 
-// int	main ()
-// {
-// 	printf("%s\n", ft_strtrim("HolaMundo", "Mundo"));
-// 	printf("%s\n", ft_strtrim("HolaMundo", "Hola"));
-
-// 	return 0;	
-// }
+	return 0;	
+}
