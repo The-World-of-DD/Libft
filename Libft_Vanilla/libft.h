5bd505fc3f327685aae329dfe455947b01eff679
DIegoR <128084117+The-World-of-DD@users.noreply.github.com>
2025-04-04 19:39:04 +0200

    jdsddd

--- Libft_Vanilla/libft.h
+++ Libft_Vanilla/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dierojas < dierojas@student.42madrid.co    +#+  +:+       +#+        */
+/*   By: dierojas <dierojas@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:40:37 by dierojas          #+#    #+#             */
-/*   Updated: 2025/04/04 12:10:27 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/04/04 19:38:40 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,12 @@ typedef	struct s_list
 }					t_list;
 
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-
+//void	ft_lstadd_front(t_list **lst, t_list *new);
+//int		ft_lstsize(t_list *lst);
+//t_list	*ft_lstlast(t_list *lst);
+//void	ft_lstadd_back(t_list **lst, t_list *new);
+//void	ft_lstdelone(t_list *lst, void (*del)(void *));
+//void	ft_lstclear(t_list **lst, void (*del)(void *));
+//void	ft_lstiter(t_list *lst, void (*f)(void *));
+//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
--- ft_matrix_free.c
+++ ft_matrix_free.c
@@ -6,13 +6,13 @@
 /*   By: dierojas <dierojas@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 21:04:39 by dierojas          #+#    #+#             */
-/*   Updated: 2025/02/03 21:19:01 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/04/04 19:11:10 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_test_mem_free(char **str, int j)
+void	ft_matrix_free(char **str, int j)
 {
 	if (!str[j])
 	{
