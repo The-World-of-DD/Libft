243aa5354e0af2dec6df88dc665cb235cd0aa6e3
DIegoR <128084117+The-World-of-DD@users.noreply.github.com>
2025-04-06 19:23:21 +0200

    jjj

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dierojas < dierojas@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:01:04 by dierojas          #+#    #+#             */
-/*   Updated: 2025/04/06 19:01:25 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/04/06 19:23:18 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	nlst;
+	t_list	nnode;
+
+	
+	while (lst)
+	{
+		(f)(lst -> content);
+		lst = lst -> next;
+	}
+	
 	
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dierojas < dierojas@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:40:37 by dierojas          #+#    #+#             */
-/*   Updated: 2025/04/06 19:00:54 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/04/06 19:10:35 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 #endif
