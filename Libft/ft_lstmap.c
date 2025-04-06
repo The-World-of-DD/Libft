9e4344ea4b3ca0338dc2a506cf884f3238eba98a
DIegoR <128084117+The-World-of-DD@users.noreply.github.com>
2025-04-06 20:02:31 +0200

    hhh

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dierojas < dierojas@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:01:04 by dierojas          #+#    #+#             */
-/*   Updated: 2025/04/06 19:23:18 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/04/06 19:58:45 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,27 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	nlst;
-	t_list	nnode;
+	t_list	*nlst;
+	t_list	*nnode;
 
-	
+	nlst = NULL;
+	nnode = NULL;
 	while (lst)
 	{
-		(f)(lst -> content);
-		lst = lst -> next;
+		nnode = malloc(sizeof(t_list));
+		if (!nnode)
+		{
+			ft_lstclear(&nlst, del);
+			return (NULL);
+		}
+	}
+	nnode -> content = f(lst -> content);
+	nnode -> next = NULL;
+	if (!nlst)
+		nlst = nnode;
+	else
+	{
+		
 	}
 	
 	
